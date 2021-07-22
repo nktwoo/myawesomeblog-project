7863f35fb8e9f39405a47367a54011ff8c8a4eaf
Nik <brnbkent2804@gmail.com>
2021-07-22 15:10:40 +0300

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=30)
+	date = models.DateTimeField()
+	text = models.CharField(max_length=300)
+	image = models.ImageField(upload_to='event_images/')
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
